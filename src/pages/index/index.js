8a80d2c31f428b9152e190c0b2ac01c347201cb5
Ligalaiz <slavakuz88@gmail.com>
2022-07-31 19:19:37 +0300

    refactor: S-5 import pagination

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,6 @@
 import '@src/layouts/layout';
 import '@components/dropdown/dropdown';
+import '@components/pagination/pagination';
 import '@utils/update-goods-count.utils';
 import '@assets/styles/global/index.scss';
 import { render } from '@utils/render.utils';
