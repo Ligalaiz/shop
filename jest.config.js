d90467865d9522c1e353c4a3c10fbc1b9ecc5984
Ligalaiz <slavakuz88@gmail.com>
2022-07-31 17:45:41 +0300

    build: S-1 change config

--- jest.config.js
+++ jest.config.js
@@ -14,8 +14,9 @@ module.exports = {
   },
   moduleNameMapper: {
     '^@src(.*)$': '<rootDir>/src$1',
-    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@utils(.*)$': '<rootDir>/src/utils$1',
     '^@assets(.*)$': '<rootDir>/src/assets$1',
+    '^@components(.*)$': '<rootDir>/src/components$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/internals/mocks/fileMock.js',
     '\\.(css|scss)$': '<rootDir>/src/internals/mocks/styleMock.js',
