8e3e4ea6007ac373927178b3fec0c703b1e41b24
Ligalaiz <slavakuz88@gmail.com>
2022-07-31 18:39:45 +0300

    refactor: S-3 import product-list

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,3 +1,9 @@
 import '@src/layouts/layout';
 import '@components/dropdown/dropdown';
+import '@utils/update-goods-count.utils';
 import '@assets/styles/global/index.scss';
+import { render } from '@utils/render.utils';
+import { ProductList } from '@components/product-list/product-list';
+
+render();
+ProductList();
